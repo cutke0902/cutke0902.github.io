ee52c0d8d7deb97f73ea171982c09a99a6958e4a
Zvone Cutura <96569958+cutke0902@users.noreply.github.com>
2024-04-05 11:31:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,8 @@
   var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });  
+  var Ortofoto = L.tileLayer('https://geoportal.dgu.hr/services/inspire/orthophoto_2021/wms?service=WMS&request=GetCapabilities'
+});  
 
 var wms1=L.tileLayer.betterWms(url, {
   layers: 'zasticeni_fosili',
